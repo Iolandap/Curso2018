347423f86b0aafef61de484f25253f572cec3c19
curs2018 <curs2018@BI101-11.epsevg.upc.es>
2018-04-13 09:18:52 +0200

    g

---
+++ Modulo 3/00 - NW.js/app/main.js
@@ -0,0 +1,23 @@
+var gui = require('nw.gui'),
+    app = gui.Window.get(),
+    nativeMenuBar = new gui.Menu({type: 'menubar'});
+
+// render native mac menus
+if (nativeMenuBar.createMacBuiltin) {
+  nativeMenuBar.createMacBuiltin('App Name', {
+    // hideEdit: true,
+    // hideWindow: true
+  });
+  app.menu = nativeMenuBar;
+}
+
+// handles cmd+q on OS X
+app.on('close', function() {
+  gui.App.quit();
+});
+
+// hide window until fully loaded
+window.addEventListener('load', function() {
+  gui.Window.get().show();
+  gui.Window.get().focus();
+});
